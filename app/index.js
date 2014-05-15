bffb9581272b1854662c98cdc12923a37b3a619c
Angus Fretwell <angusfretwell@me.com>
2014-05-15 23:14:27 +1000

    Update Gulpfile

--- app/index.js
+++ app/index.js
@@ -1,104 +1,141 @@
 'use strict';
-
+var fs = require('fs');
 var util = require('util');
 var path = require('path');
 var spawn = require('child_process').spawn;
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var wiredep = require('wiredep');
 
 var FrancisCraftGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+    init: function () {
+        this.pkg = require('../package.json');
+    },
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
+    askFor: function () {
+        var done = this.async();
 
-  askFor: function () {
-    var done = this.async();
+        this.log(this.yeoman);
+        this.log(chalk.magenta('You\'re using Francis Bond\'s fantastic Craft generator.'));
 
-    this.log(this.yeoman);
-    this.log(chalk.magenta('You\'re using Francis Bond\'s fantastic Craft generator.'));
+        var prompts = [{
+            name: 'slug',
+            message: 'Please enter a unique slug for this project',
+        }, {
+            type: 'checkbox',
+            name: 'features',
+            message: 'What more would you like?',
+            choices: [{
+                name: 'inuit.css',
+                value: 'includeInuit',
+                checked: true
+            }, {
+                name: 'jQuery',
+                value: 'includejQuery',
+                checked: true
+            }]
+        }];
 
-    var prompts = [{
-      name: 'slug',
-      message: 'Please enter a unique slug for this project',
-    }];
+        this.prompt(prompts, function (props) {
+            this.slug = props.slug;
 
-    this.prompt(prompts, function (props) {
-      this.slug = props.slug;
+            var features = answers.features;
 
-      done();
-    }.bind(this));
-  },
+            var hasFeature = function (feat) {
+                return features.indexOf(feat) !== -1;
+            }
 
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/styles');
-    this.mkdir('app/scripts');
-    this.mkdir('app/images');
-    this.mkdir('app/webfonts');
-    this.mkdir('app/templates');
+            this.includeInuit = hasFeature('includeInuit');
+            this.includejQuery = hasFeature('includejQuery');
 
-    this.mkdir('public');
-    this.mkdir('public/assets');
-  },
+            done();
+        }.bind(this));
+    },
 
-  projectfiles: function () {
-    this.copy('_package.json', 'package.json');
-    this.copy('_composer.json', 'composer.json');
+    app: function () {
+        this.mkdir('app');
+        this.mkdir('app/styles');
+        this.mkdir('app/scripts');
+        this.mkdir('app/images');
+        this.mkdir('app/webfonts');
+        this.mkdir('app/templates');
 
-    this.copy('bowerrc', '.bowerrc');
-    this.copy('_bower.json', 'bower.json');
+        this.mkdir('public');
+        this.mkdir('public/assets');
+    },
 
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshint', '.jshint');
+    projectfiles: function () {
+        this.copy('_package.json', 'package.json');
+        this.copy('_composer.json', 'composer.json');
 
-    this.copy('gitignore', '.gitignore');
-    this.copy('gitattributes', '.gitattributes');
+        this.copy('bowerrc', '.bowerrc');
+        this.copy('_bower.json', 'bower.json');
 
-    this.copy('favicon.ico', 'app/favicon.ico');
-    this.copy('robots.txt', 'app/robots.txt');
-    this.copy('humans.txt', 'app/humans.txt');
+        this.copy('editorconfig', '.editorconfig');
+        this.copy('jshint', '.jshint');
 
-    this.copy('htaccess', 'app/.htaccess');
-  },
+        this.copy('gitignore', '.gitignore');
+        this.copy('gitattributes', '.gitattributes');
 
-  vagrant: function () {
-    this.copy('Vagrantfile', 'Vagrantfile');
-  },
+        this.copy('favicon.ico', 'app/favicon.ico');
+        this.copy('robots.txt', 'app/robots.txt');
+        this.copy('humans.txt', 'app/humans.txt');
 
-  puppet: function () {
-    this.mkdir('puppet');
-    this.mkdir('puppet/manifests');
-    this.mkdir('puppet/modules');
-    this.mkdir('puppet/modules/app');
-    this.mkdir('puppet/modules/app/manifests');
+        this.copy('htaccess', 'app/.htaccess');
+    },
 
-    this.write('puppet/manifests/init.pp', 'include \'app\'');
-    this.copy('init.pp', 'puppet/modules/app/manifests/init.pp');
+    vagrant: function () {
+        this.copy('Vagrantfile', 'Vagrantfile');
+    },
 
-    this.directory('bootstrap', 'puppet/boostrap');
+    puppet: function () {
+        this.mkdir('puppet');
+        this.mkdir('puppet/manifests');
+        this.mkdir('puppet/modules');
+        this.mkdir('puppet/modules/app');
+        this.mkdir('puppet/modules/app/manifests');
 
-    this.directory('apache', 'puppet/modules/apache');
-    this.directory('mysql', 'puppet/modules/mysql');
-    this.directory('php', 'puppet/modules/php');
-    this.directory('git', 'puppet/modules/git');
-    this.directory('curl', 'puppet/modules/curl');
-    this.directory('composer', 'puppet/modules/composer');
-  },
+        this.write('puppet/manifests/init.pp', 'include \'app\'');
+        this.copy('init.pp', 'puppet/modules/app/manifests/init.pp');
 
-  craft: function() {
-    this.mkdir('craft');
-    this.mkdir('craft/config');
-    this.mkdir('craft/storage');
+        this.directory('bootstrap', 'puppet/boostrap');
 
-    this.copy('general.php', 'craft/config/general.php');
-    this.copy('db.php', 'craft/config/db.php');
-  }
+        this.directory('apache', 'puppet/modules/apache');
+        this.directory('mysql', 'puppet/modules/mysql');
+        this.directory('php', 'puppet/modules/php');
+        this.directory('git', 'puppet/modules/git');
+        this.directory('curl', 'puppet/modules/curl');
+        this.directory('composer', 'puppet/modules/composer');
+    },
+
+    craft: function() {
+        this.mkdir('craft');
+        this.mkdir('craft/config');
+        this.mkdir('craft/storage');
+
+        this.copy('index.php', 'public/index.php')
+
+        this.copy('general.php', 'craft/config/general.php');
+        this.copy('db.php', 'craft/config/db.php');
+    },
+
+    install: function() {
+        var done = this.async();
+
+        this.installDependencies({
+            callback: function () {
+                var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
+
+                wiredep({
+                    bowerJson: bowerJson,
+                    directory: 'app/bower_components',
+                    src: 'app/index.html'
+                });
+
+                done();
+            }.bind(this);
+        });
+    }
 });
 
 module.exports = FrancisCraftGenerator;
--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,18 +3,6 @@
 
 var gulp = require('gulp');
 
-// Load plugins
-var $ = require('gulp-load-plugins')();
-
-var paths = {
-    styles: 'app/styles/**/*.scss',
-    scripts: 'app/scripts/**/*.js',
-    html: 'app/*.html',
-    images: 'app/images/**/*',
-    fonts: 'app/webfonts/**/*.{eot,svg,ttf,woff}',
-    extras: ['app/*.*', '!app/*.html']
-}
-
 /**
  * `gulp deploy-init`
  * 1. Create mariadb container.
@@ -38,48 +26,80 @@ gulp.task('db-dump-local', shell.task([
 ]));
 
 gulp.task('db-dump-remote', shell.task([
-    'ssh dokku@staging.francisbond.com mariadb:dumpraw <%= _.slugify(slug) %> > .tmp/local.sql'
+    'ssh dokku@staging.francisbond.com mariadb:dumpraw <%= _.slugify(slug) %> > .tmp/remote.sql'
 ]));
-//
-//gulp.task('db-push', function () {
-//
-//});
-//
-//gulp.task('db-pull', function () {
-//
-//});
-//
-//gulp.task('db-dump', function () {
-//
-//});
 
-gulp.task('styles', function () {
-    return gulp.src(paths.styles)
+gulp.task('db-push', ['clean', 'db-dump-local'] function () {
+    'ssh dokku@staging.francisbond.com mariadb:console <%= _.slugify(slug) %> < .tmp/local.sql'
+});
+
+gulp.task('db-pull', ['clean', 'db-dump-remote'] function () {
+    'vagrant ssh "mysql -uroot -proot <%= _.slugify(slug) %>" < .tmp/remote.sql'
+});
+
+gulp.task('db-dump', ['clean', 'db-dump-local', 'db-dump-remote'] function () {
+    return gulp.src(['.tmp/local.sql', '.tmp/remote.sql'])
+        .pipe(gulp.dest('databases'));
+});
+
+gulp.task('styles', function() {
+    return gulp.src('app/styles/**/*.scss')
         .pipe($.rubySass({
             style: 'expanded'
         }))
         .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('.tmp/styles'))
+        .pipe(gulp.dest('public/styles'))
         .pipe($.size());
 });
 
-gulp.task('scripts', function () {
-    return gulp.src(paths.scripts)
+gulp.task('scripts', function() {
+    return gulp.src('app/scripts/**/*.js')
         .pipe($.jshint())
         .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe(gulp.dest('public/styles'))
         .pipe($.size());
 });
 
-gulp.task('html', ['styles', 'scripts'], function () {
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('public'))
+});
+
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.{gif,jpg,png,svg,webp}')
+        .pipe(cache(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('public/images'))
+        .pipe(size());
+});
+
+gulp.task('extras', function() {
+    return gulp.src(['app/*.*', '!app/*.html', 'app/webfonts/**/*.{eot,svg,ttf,woff}'], { dot: true })
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(['.tmp', 'public', '!public/assets', '!public/index.php'], { read: false })
+        .pipe(clean());
+});
+
+gulp.task('build', ['clean', 'images', 'extras'], function() {
     var jsFilter = $.filter('**/*.js');
     var cssFilter = $.filter('**/*.css');
 
-    return gulp.src(paths.html)
-        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+    return gulp.src('app/*.html')
+        .pipe($.useref.assets({searchPath: '{public,app}'}))
         .pipe(jsFilter)
         .pipe($.uglify())
         .pipe(jsFilter.restore())
         .pipe(cssFilter)
+        .pipe($.rubySass({
+            style: 'expanded'
+        }))
+        .pipe($.autoprefixer('last 1 version'))
         .pipe($.csso())
         .pipe(cssFilter.restore())
         .pipe($.useref.restore())
@@ -88,36 +108,17 @@ gulp.task('html', ['styles', 'scripts'], function () {
         .pipe($.size());
 });
 
-gulp.task('images', function () {
-    return gulp.src(paths.images)
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('public/images'))
-        .pipe($.size());
+gulp.task('watch', function () {
+    // start livereload
+
+    gulp.watch(['app/*.*', '!app/*.html', 'app/webfonts/**/*.{eot,svg,ttf,woff}'], ['extras']);
+    gulp.watch(['app/*.html', ['html']);
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('app/images/**/*.{gif,jpg,png,svg,webp}', ['images']);
+    gulp.watch('bower.json', ['wiredep']);
 });
 
-gulp.task('fonts', function () {
-    return gulp.src(paths.fonts)
-        .pipe(gulp.dest('dist/webfonts'))
-        .pipe($.size());
-});
-
-gulp.task('extras', function () {
-    return gulp.src(paths.extras, { dot: true })
-        .pipe(gulp.dest('public'));
-});
-
-gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'public', '!public/assets'], { read: false })
-        .pipe($.clean());
-});
-
-gulp.task('build', ['html', 'images', 'fonts', 'extras']);
-
-// Wire Bower dependencies
 gulp.task('wiredep', function () {
     var wiredep = require('wiredep').stream;
 
@@ -128,12 +129,6 @@ gulp.task('wiredep', function () {
         .pipe(gulp.dest('app'));
 });
 
-// Watch for changes
-gulp.task('watch', ['build', 'connect'], function () {
-    gulp.watch('app/**/*', ['build']);
-});
-
-// Default task
-gulp.task('default', ['clean'], function () {
+gulp.task('default', function () {
     gulp.start('build');
 });
