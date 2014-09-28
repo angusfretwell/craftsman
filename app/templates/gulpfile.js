cc174a277da18e3cb939fde795d3ebea1b1bc406
Angus Fretwell <angusfretwell@me.com>
2014-09-28 21:29:02 +1000

    Tidy up gulpfile

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -17,14 +17,13 @@ var paths = {
 };
 
 /**
- * `gulp deploy-init`
  * 1. Add dokku remote to git repository.
  * 2. Push to dokku to intialize app container.
  * 3. Set a custom PHP buildpack
  * 4. Create mariadb container.
  * 5. Link app container with mariadb container.
  */
-gulp.task('deploy-init', shell.task([
+gulp.task('deploy-init', $.shell.task([
   'git remote add dokku dokku@staging.francisbond.com:<%= _.slugify(slug) %>', /*[1]*/
   'git push dokku master', /*[2]*/
   'ssh dokku@staging.francisbond.com config:set <%= _.slugify(slug) %> BUILDPACK_URL=https://github.com/CHH/heroku-buildpack-php', /*[3]*/
@@ -32,35 +31,33 @@ gulp.task('deploy-init', shell.task([
   'ssh dokku@staging.francisbond.com mariadb:link <%= _.slugify(slug) %> <%= _.slugify(slug) %>' /*[5]*/
 ]));
 
-gulp.task('deploy', shell.task([
+gulp.task('deploy', $.shell.task([
   'git push dokku master'
 ]));
 
 /**
- * `gulp db-dump-local`
  * 1. Check if .tmp exists, create it if it doesn't.
  * 2. Dump the local database to .tmp/local.sql.
  */
-gulp.task('db-dump-local', ['build'], shell.task([
+gulp.task('db-dump-local', ['build'], $.shell.task([
   '[ -d ".tmp" ] || mkdir .tmp', /*[1]*/
   'vagrant ssh --command "mysqldump -uroot -proot <%= _.slugify(slug) %> > /vagrant/.tmp/local.sql"' /*[2]*/
 ]));
 
 /**
- * `gulp db-dump-remote`
  * 1. Check if .tmp exists, create it if it doesn't.
  * 2. Dump the remote database to .tmp/remote.sql.
  */
-gulp.task('db-dump-remote', ['build'], shell.task([
+gulp.task('db-dump-remote', ['build'], $.shell.task([
   '[ -d ".tmp" ] || mkdir .tmp', /*[1]*/
   'ssh dokku@staging.francisbond.com mariadb:dumpraw <%= _.slugify(slug) %> | tee .tmp/remote.sql > /dev/null' /*[2]*/
 ]));
 
-gulp.task('db-push', ['db-dump-local'], shell.task([
+gulp.task('db-push', ['db-dump-local'], $.shell.task([
   'ssh dokku@staging.francisbond.com mariadb:console <%= _.slugify(slug) %> < .tmp/local.sql'
 ]));
 
-gulp.task('db-pull', ['db-dump-remote'], shell.task([
+gulp.task('db-pull', ['db-dump-remote'], $.shell.task([
   'vagrant ssh --command "mysql -uroot -proot <%= _.slugify(slug) %> < /vagrant/.tmp/remote.sql"'
 ]));
 
@@ -77,27 +74,24 @@ gulp.task('styles', function() {
       precision: 10
     }))
     .pipe($.autoprefixer('last 1 version'))
-    .pipe(prefix('last 1 version'))
     .pipe(gulp.dest('public/styles'))
 });
 
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
-    .pipe(jshint())
+    .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.jshint.reporter('fail'))
     .pipe(gulp.dest('public/scripts'));
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(paths.images)
-    .pipe(cache(imagemin({
-      optimizationLevel: 3,
+    .pipe($.cache($.imagemin({
       progressive: true,
       interlaced: true
     })))
-    .pipe(gulp.dest('public/images'))
-    .pipe(size());
+    .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('extras', function() {
@@ -118,17 +112,16 @@ gulp.task('build-useref', ['images', 'styles', 'extras'], function() {
   var cssFilter = filter('**/*.css');
 
   return gulp.src(paths.html)
-    .pipe(useref.assets({searchPath: '{public,app}'}))
-    .pipe(jsFilter)
-    .pipe(uglify())
-    .pipe(jsFilter.restore())
-    .pipe(cssFilter)
-    .pipe(csso())
-    .pipe(cssFilter.restore())
-    .pipe(useref.restore())
-    .pipe(useref())
-    .pipe(gulp.dest('public'))
-    .pipe(size());
+    .pipe($.useref.assets({searchPath: '{public,app}'}))
+    .pipe($.jsFilter)
+    .pipe($.uglify())
+    .pipe($.jsFilter.restore())
+    .pipe($.cssFilter)
+    .pipe($.csso())
+    .pipe($.cssFilter.restore())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('watch', function() {
@@ -141,7 +134,7 @@ gulp.task('watch', function() {
 
   livereload.listen();
   gulp.watch('public/**/*')
-    .on('change', livereload.changed);
+    .on('change', $.livereload.changed);
 
   gulp.watch(paths.extras, ['extras']);
   gulp.watch(paths.html, ['build-useref']);
